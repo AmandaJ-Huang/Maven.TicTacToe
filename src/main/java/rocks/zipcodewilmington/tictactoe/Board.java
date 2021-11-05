0dbee3515d53e1c5e12d0044a77e3614c60f5ea9
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-11-04 23:29:29 -0400

    inFavorOfPlayerX row done?

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,11 +4,24 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    private Character[][] board;
     public Board(Character[][] matrix) {
+        this.board = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        for(int row = 0; row < 3; row++) {
+            int checkCounterX = 0;
+            for(int col = 0; col < 3; col++) {
+                if(board[row][col].equals('X')) {
+                    checkCounterX++;
+                    }
+                }
+            if (checkCounterX == 3) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public Boolean isInFavorOfO() {
